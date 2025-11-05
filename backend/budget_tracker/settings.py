e5706d895c9c1293ecf9becd8fbca48911545587
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2025-11-05 19:25:29 +0530

    Update settings.py

--- backend/budget_tracker/settings.py
+++ backend/budget_tracker/settings.py
@@ -4,7 +4,7 @@ Django settings for budget_tracker project.
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,10 +74,7 @@ WSGI_APPLICATION = 'budget_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
